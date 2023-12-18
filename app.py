249d27bad32700f38bdfa65e31973187fe560398
Erendiria <151480543+Erendiria@users.noreply.github.com>
2023-12-18 15:55:34 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_jwt_extended import JWTManager 
 from flask_restful import Api
 from config import Config
-from resources.naver import ChineseResource, NewsResourec
+from resources.naver import ChineseResource, NewsResource
 
 app = Flask(__name__)
 
@@ -14,7 +14,7 @@ jwt = JWTManager(app)
 api = Api(app)
 
 api.add_resource( ChineseResource , '/chinese')
-api.add_resource( NewsResourec , '/news')
+api.add_resource( NewsResource , '/news')
 
 if __name__ == '__main__':
     app.run()
