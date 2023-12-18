1f582cee45a719369cd0ba1cb0b8ebc5e555598e
Erendiria <151480543+Erendiria@users.noreply.github.com>
2023-12-18 15:55:30 +0900

    Update utils.py

--- utils.py
+++ utils.py
@@ -15,3 +15,4 @@ def check_password(original_password, hashed_password):
     original_password = original_password + Config.PASSWORD_SALT
     check = pbkdf2_sha256.verify(original_password, hashed_password)
     return check
+
